d213cb53506dcd81e8a0e6e6d35bbf89c1d959a9
15138730370 <1037483576@qq.com>
2017-12-15 09:39:36 +0800

    测试notification成功

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+declare const cordova: any;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  hasPermission: boolean = true;
+  constructor(
+    public navCtrl: NavController
+  ) {
 
-  constructor(public navCtrl: NavController) {
+  }
 
+  test() {
+    cordova.plugins.notification.local.hasPermission((granted) => {
+      this.hasPermission = granted;
+      if (!this.hasPermission) {
+        cordova.plugins.notification.local.requestPermission(function (granted) {
+          this.hasPermission = granted;
+        });
+      }
+      if(this.hasPermission){
+        this.send();
+      }
+    });
+  }
+
+  send() {
+    cordova.plugins.notification.local.schedule({
+      title: 'localNotifications',
+      text: 'text',
+      badge: 10,
+      at: new Date(new Date().getTime() + 3600)
+    });
   }
 
 }
